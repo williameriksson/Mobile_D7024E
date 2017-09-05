ad329eb26989c9305656b910fd0e0a820f63b5c8
William Eriksson <wileri-2@student.ltu.se>
2017-09-05 22:38:39 +0200

    Added JSON marshalling

--- d7024e/network.go
+++ d7024e/network.go
@@ -3,11 +3,14 @@ package d7024e
 import (
 	"fmt"
 	"net"
-	"time"
-	"strconv"
+	"encoding/json"
 )
 
 type Network struct {
+}
+
+type Message struct {
+	Command string
 }
 
 func checkError(err error) {
@@ -17,58 +20,56 @@ func checkError(err error) {
 }
 
 func (network *Network) Listen() {
-	serverAddr, err := net.ResolveUDPAddr("udp",":8000")
+	serverAddr, err := net.ResolveUDPAddr("udp",":8002")
 	conn, err := net.ListenUDP("udp", serverAddr)
 	checkError(err)
 
-	fmt.Println("1")
-	channel1 := make(chan string)
-	go network.handleConnection(conn, channel1)
+	go network.handleConnection(conn)
 	go network.test()
 	for {
-		msg := <-channel1
-		fmt.Println(msg)
+
 	}
-	fmt.Println("2")
+
 }
 
-func (network *Network) handleConnection(conn *net.UDPConn, channel1 chan string) {
+func (network *Network) handleConnection(conn *net.UDPConn) {
 	buf := make([]byte, 1024)
-	fmt.Println("3")
+	var msg Message
+
 	defer conn.Close()
 	for {
 		n, addr, err := conn.ReadFromUDP(buf)
 		checkError(err)
-		receivedString := string(buf[0:n])
-		fmt.Println(n, addr, receivedString)
-		channel1 <- receivedString
+		err = json.Unmarshal(buf[0:n], &msg)
+		checkError(err)
+
+		switch msg.Command {
+		case "PING":
+			fmt.Println("GOT PING")
+		default:
+			fmt.Println("GOT DEFAULT", n, addr, msg)
+		}
 	}
-	fmt.Println("4")
+
 }
 
 func (network *Network) test() {
-		fmt.Println("5")
-    ServerAddr,err := net.ResolveUDPAddr("udp",":8000")
+    ServerAddr,err := net.ResolveUDPAddr("udp",":8002")
     checkError(err)
 
-    LocalAddr, err := net.ResolveUDPAddr("udp", ":8001")
+    LocalAddr, err := net.ResolveUDPAddr("udp", ":8003")
     checkError(err)
 
     Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
     checkError(err)
 
     defer Conn.Close()
-    i := 0
-    for {
-        msg := strconv.Itoa(i)
-        i++
-        buf := []byte(msg)
-        _,err := Conn.Write(buf)
-        if err != nil {
-            fmt.Println(msg, err)
-        }
-        time.Sleep(time.Second * 1)
-    }
+
+		msg := Message{"PING"}
+		marshMsg, err := json.Marshal(msg)
+		checkError(err)
+		Conn.Write(marshMsg)
+
 }
 
 /*
