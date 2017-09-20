bcbefb8186773f49adf7a4bf74fb2a29fadd8a35
David Sandstrom <davsan-0@student.ltu.se>
2017-09-20 22:54:06 +0200

    Test Store

--- d7024e/kademlia_test.go
+++ d7024e/kademlia_test.go
@@ -3,7 +3,8 @@ package d7024e_test
 import (
 	"Mobile_D7024E/d7024e"
 	"testing"
-	"fmt"
+	//"fmt"
+	"time"
 )
 
 const timeOut time.Duration = (2 * time.Second)
@@ -19,6 +20,30 @@ func TestKademliaInstantiation(t *testing.T) {
 	}
 }
 
+func TestStore(t *testing.T) {
+	kademlia1 := d7024e.NewKademlia()
+	go kademlia1.Run("", "127.0.0.1:8000")
+
+	time.Sleep(time.Millisecond * 1000)
+
+	kademlia2 := d7024e.NewKademlia()
+	go kademlia2.Run("127.0.0.1:8000", "127.0.0.1:8002")
+
+	time.Sleep(time.Millisecond * 1000)
+
+	data := []byte("Test String")
+	hash := d7024e.HashData(data)
+	nodes := kademlia2.RoutingTable.FindClosestNodes(d7024e.NewKademliaID(hash), 1)
+	//fmt.Printf("node: %x\n", nodes[0].ID)
+	kademlia2.Network.SendStoreMessage(&nodes[0], data)
+
+	time.Sleep(time.Millisecond * 1000)
+
+	kademlia1.LookupValue(hash)
+
+	time.Sleep(time.Millisecond * 1000)
+}
+/*
 func TestKademlia(t *testing.T) {
 	kademlia1 := d7024e.NewKademlia()
 	go kademlia1.Run("", "127.0.0.1:8000")
@@ -44,12 +69,7 @@ func TestKademlia(t *testing.T) {
 	go kademlia5.Run("127.0.0.1:8000", "127.0.0.1:8005")
 
 	time.Sleep(time.Millisecond * 1000)
-	//
-	/* Davids Test Code */
-	// data := d7024e.HashStr("Teststring")
-	// kademlia.Store(data)
-	// kademlia.LookupData(d7024e.HashData(data))
-	/* End of Davids Test Code */
+	
 	for {
 		select {
 		case msg := <-kademlia1.Network.TestChannel:
@@ -64,4 +84,4 @@ func TestKademlia(t *testing.T) {
 			fmt.Println(msg)
 		}
 	}
-}
+}*/
